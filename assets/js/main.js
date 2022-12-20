e1bef44eec2c0ad90078a3e177a22ac9b3ea8220
uhhpage <uhhpage2@gmail.com>
2022-12-20 16:04:33 +0900

    8

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,7 +127,7 @@
 
 		// Toggle.
 			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+				//'<a href="#navPanel" id="navPanelToggle">Menu</a>'
 			)
 				.appendTo($wrapper);
 
